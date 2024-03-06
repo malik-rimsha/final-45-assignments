2a522a1a70a7586d52159d316022e2eb070e6c97
DUA-UMER <duam95623@gmail.com>
2024-03-06 23:48:28 +0500

    first commit

---
+++ assign03/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its author
+
+let quote:string ="think 100 times before you take a decision, but once that decision is taken, stand by it as one men"
+
+let author:string = "Muhammad Ali Jinnah"
+
+// printing the message
+
+console.log(`"${quote}" - ${author}`);
