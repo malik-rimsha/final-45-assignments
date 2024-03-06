8cfcd7e6faf1bea7153780cd5c88ad7616212098
DUA-UMER <duam95623@gmail.com>
2024-03-06 23:39:55 +0500

    first commit

---
+++ assign02/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// storing a person name in a variable
+let personName = "rimsha umer";
+// printing a person name in lowercase
+console.log("Lowercase:", personName.toLowerCase());
+// printing a person name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+// printing a personName in Titlecase
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1, 11));
